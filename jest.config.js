cb063310ac25a4db9b570d7026f7dcb52bdac861
gg-redkite-aqa <grishaghlijyan@mail.ru>
2023-04-11 13:58:55 +0400

    versions change

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    preset: "jest-puppeteer",
-    testTimeout: 60000,
-    globals: { URL: "https://redkite.io/en" },
-    verbose: true
+  preset: 'jest-puppeteer',
+  testTimeout: 60000,
+  globals: { URL: 'https://redkite.io/en' },
+  verbose: true,
 };
--- tests/task_001.test.js
+++ tests/task_001.test.js
@@ -1,28 +1,22 @@
 import { homePage } from '../pages/homepage';
-import { aboutUs } from '../pages/aboutUs'
+import { aboutUs } from '../pages/aboutUs';
 
-describe("Hompage and maintabs", () => {
-  beforeEach(async () => {
-    page.setDefaultNavigationTimeout(0)
-  });
+describe('Hompage and maintabs', () => {
+  beforeEach(async () => page.setDefaultNavigationTimeout(0));
 
   test('Maintabs should exist and be clickable on homepage', async () => {
     await page.goto(URL);
     await page.waitForSelector(homePage.maintabsSelector);
-    let tabNames = await page.$$eval(homePage.maintabsSelector, tabsArray => {
-      return tabsArray.map(tab => tab.textContent)
-    });
+    const tabNames = await page.$$eval(homePage.maintabsSelector, tabsArray => tabsArray.map(tab => tab.textContent));
     await expect(tabNames).toEqual(homePage.maintabs);
   });
 
   test('About us page should contain stuff linkedin links', async () => {
     await page.goto(`${URL}/about`);
     await page.waitForSelector(aboutUs.stufflinks);
-    let stuffLinkedinLinks = await page.$$eval(aboutUs.stufflinks, linksArray => {
-      return linksArray.map(link => link.href)
-    });
+    const stuffLinkedinLinks = await page.$$eval(aboutUs.stufflinks, linksArray => linksArray.map(link => link.href));
     stuffLinkedinLinks.forEach(link => {
-      expect(link).toContain('https://www.linkedin.com')
-    })
-  })
+      expect(link).toContain('https://www.linkedin.com');
+    });
+  });
 });
